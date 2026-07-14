c7fae15093b5f2d31b208961dae16ad5a0a591dd
Szymon Balicki <sbalicki@student.agh.edu.pl>
2026-07-14 14:16:42 +0200

    fix: formatting

--- app/api/routes/products.py
+++ app/api/routes/products.py
@@ -12,12 +12,17 @@ products_file_path = "app/api/mock_products.json"
 with open(products_file_path, encoding="utf-8") as f:
     products = json.load(f)["products"]
 
+
 @router.get("/")
 async def get_products(filter: str = None, page: int = 1):
     if filter:
         products_list = jsonable_encoder(
-            [product for product in products \
-             if filter.lower() in product['name'].lower()])
+            [
+                product
+                for product in products
+                if filter.lower() in product["name"].lower()
+            ]
+        )
     if page:
         start_index = (page - 1) * PAGE_SIZE
         end_index = start_index + PAGE_SIZE
@@ -26,13 +31,16 @@ async def get_products(filter: str = None, page: int = 1):
         products_list = jsonable_encoder(products)
     return products_list
 
+
 @router.get("/{product_id}")
 async def get_product(product_id: int):
     product = next(
-        (product for product in products if product["id"] == product_id), None)
+        (product for product in products if product["id"] == product_id), None
+    )
     if product:
         return jsonable_encoder(product)
     return {"error": "Product not found"}
+
 
 @router.get("/{product_id}/availability")
 async def get_product_availability(product_id: int, start_date: str, end_date: str):
