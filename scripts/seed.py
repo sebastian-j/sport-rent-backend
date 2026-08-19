84ed5224136098ef326e5a51ea47086b1c6ac71f
Jacob898 <jstach898@gmail.com>
2026-08-19 14:51:56 +0200

    ruff fix

--- scripts/seed.py
+++ scripts/seed.py
@@ -1,12 +1,11 @@
 import asyncio
 
 from app.db.session import engine
-from scripts.seeds import seed_subcategories
 from scripts.seeds.seed_favorites import seed_favorites
 from scripts.seeds.seed_orders import seed_orders
 from scripts.seeds.seed_products import seed_products
-from scripts.seeds.seed_users import seed_users
 from scripts.seeds.seed_subcategories import seed_subcategories
+from scripts.seeds.seed_users import seed_users
 
 
 async def main() -> None:
