e2769570954b2beeb9eb4f38b0662131890e69f1
Jacob898 <jstach898@gmail.com>
2026-08-20 13:47:23 +0200

    Add subcategories to API

--- app/schemas/category.py
+++ app/schemas/category.py
@@ -1,7 +1,10 @@
 from pydantic import BaseModel
 
+from app.schemas.subcategory import SubcategoryResponse
+
 
 class CategoryResponse(BaseModel):
     name: str
     image: str | None = None
     slug: str
+    subcategories: list[SubcategoryResponse]
